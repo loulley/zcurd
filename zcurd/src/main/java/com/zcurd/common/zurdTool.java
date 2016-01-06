eecf442cffed9ee156f60eb36c8d6c710a0c3445
钟世云 <515097842@qq.com>
2016-01-06 19:17:15 +0800

    生成表单

---
+++ zcurd/src/main/java/com/zcurd/common/zurdTool.java
@@ -0,0 +1,19 @@
+package com.zcurd.common;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.jfinal.plugin.activerecord.Record;
+
+public class zurdTool {
+	
+	public static List<Map<String, Object>> listRecord2ListMap(List<Record> listRecord) {
+		List<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
+		for (Record record : listRecord) {
+			list.add(record.getColumns());
+		}
+		return list;
+	}
+
+}
